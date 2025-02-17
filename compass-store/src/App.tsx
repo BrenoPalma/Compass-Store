84efc324f1a95ae274a340538f00bcc63b2daee5
Breno Palma <brenomiele@ufu.br>
2025-02-17 12:58:06 -0300

    chore: estrutura inicial do projeto

---
+++ compass-store/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './assets/logo.svg';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Meu Projeto Fake Store API</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ compass-store/src/routes.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+const AppRoutes = () =>{
+    return(
+        <Router>
+            <Routes>
+
+            </Routes>
+        </Router>
+    )
+}
+
