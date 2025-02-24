14c5e7f66eb4ff8306ec181bb3e4f73892a47fad
Breno Palma <brenomiele@ufu.br>
2025-02-24 11:20:04 -0300

    feat: Routes

--- compass-store/src/App.tsx
+++ compass-store/src/App.tsx
@@ -1,16 +1,6 @@
-import React from 'react';
-import logo from './assets/logo.svg';
-import './styles/App.css';
-
+import { AppRoutes } from "./routes";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Meu Projeto Fake Store API</h1>
-      </header>
-    </div>
-  );
+return <AppRoutes />;
 }
 
 export default App;
--- compass-store/src/routes.tsx
+++ compass-store/src/routes.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
 
-const AppRoutes = () =>{
+export const AppRoutes = () =>{
     return(
         <Router>
             <Routes>
-
+                <Route path="/" element={<Home />} />
             </Routes>
         </Router>
     )
 }
+
 
